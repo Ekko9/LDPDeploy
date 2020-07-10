4a5906cf1c792c443d5261d930c4106adad16a5a
Ekko9 <keep1024@sina.com>
2020-07-10 16:45:12 +0800

    Add packaging and deployment operations

--- DeployPro.sh
+++ DeployPro.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #deploy PRO LDPPlatform
 jarName=LDPPlatform-0.0.1.jar
-appDir=/DATA/jenkins/workspace/LDP
-jarDir=/DATA/jenkins/workspace/LDP/target
+appDir=/DATA/jenkins/workspace/LDP-Deploy-PRO
+jarDir=/DATA/jenkins/workspace/LDP-Deploy-PRO/target
 backupdir=/DATA/earBak/$(date "+%Y-%m-%d")
 
 #Determine whether the packing directory exists
@@ -57,4 +57,46 @@ echo -e "\033[32m [INFO] -------------------------------------------------------
 cd ${jarDir} && ls
 echo -e "\033[32m [INFO] ------------------------------------------------------------------------ \033[0m"
 
+sleep 3
+#Deploy application
+echo -e "\033[32m +++++++++++++++++Start backup app+++++++++++++++++ \033[0m"
+uname=root
+appBackdir=/opt/app/earPool/earBak/$(date "+%Y-%m-%d")
+appDir=/opt/app/java_apps/ldp/lib
+for i in iplist
+do
+        echo $i
+        ssh -T ${uname}@$i <<EOF
+        if [ ! -d "${appBackdir}" ]; then
+                echo -e "\033[31m directory does not exist, creating。。。 \033[0m"      
+                mkdir ${appBackdir}
+		mv ${appDir}/${jarName} ${appBackdir} 
+		cd ${appBackdir} && ls
+		echo -e "\033[32m +++++++++++++++++backup successful+++++++++++++++++ \033[0m"
+        else
+                echo -e "\033[32m directory already exists,start backup files。。。 \033[0m"
+		mv ${appDir}/${jarName} ${appBackdir}
+		cd ${appBackdir} && ls	
+  		echo -e "\033[32m +++++++++++++++++backup successful+++++++++++++++++ \033[0m"
+        fi
+                   
+EOF
+	echo -e "\033[32m +++++++++++++++++Start deploying $i app+++++++++++++++++ \033[0m"
+	scp ${jarDir}/${jarName} ${uname}@$i:${appDir}
+	ssh -T ${uname}@$i <<EOF
+	cd ${appDir}
+	if [ ! -f "${jarName}" ]; then
+		echo -e "\033[32m +++++++++++++++++${i}_${jarName} not exist，Please check+++++++++++++++++ \033[0m"
+		exit 1
+	else
+		echo -e "\033[32m +++++++++++++++++${i}_${jarName} exist， Deployment successful+++++++++++++++++ \033[0m"
+		ls
+		echo -e "\033[32m +++++++++++++++++${i}_Restarting+++++++++++++++++ \033[0m"
+		sh /opt/app/java_apps/bin/restart.sh
+		echo -e "\033[32m +++++++++++++++++${i}_restart successful+++++++++++++++++ \033[0m"	
+	fi
 
+EOF
+
+done
+exit
